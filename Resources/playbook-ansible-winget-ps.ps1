9ced238b6b2a7078782d19ab13f8e74546e4374f
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-03 23:54:05 +0300

    added update functionality

--- Resources/playbook-ansible-winget-ps.ps1
+++ Resources/playbook-ansible-winget-ps.ps1
@@ -37,11 +37,34 @@ function Uninstall-Package {
     }
 }
 
+function Update-Package {
+    param (
+        [string]$packageName
+    )
+
+    Write-Host "Updating package $packageName..."
+    winget update --id $packageName --silent
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "Package $packageName updated successfully."
+    } elseif ($LASTEXITCODE -eq -1978335189) {
+        Write-Output "Already updated."
+    } elseif ($LASTEXITCODE -eq -1978335212) {
+        Write-Output "This package is not installed."
+    } else {
+        Write-Host "Failed to update package $packageName."
+    }
+}
+
+
+
 if ($action -eq "install") {
     Install-Package -packageName $package
 } elseif ($action -eq "uninstall") {
     Uninstall-Package -packageName $package
+} elseif ($action -eq "update") {
+    Update-Package -packageName $package
 } else {
-    Write-Host "Invalid action. Use 'install' or 'uninstall'."
+    Write-Host "Invalid action. Use 'install', 'uninstall' or 'update'."
 }
 
+echo $LASTEXITCODE
