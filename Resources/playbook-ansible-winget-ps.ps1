8e0b8636df641ccfd56016ea8a69944d1f05ec80
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-05 19:16:04 +0300

    test

--- Resources/playbook-ansible-winget-ps.ps1
+++ Resources/playbook-ansible-winget-ps.ps1
@@ -56,14 +56,14 @@ function Install-Package {
     Write-Output "Installing package $packageID..."
     if (Check_If_Installed -packageID $packageID) {
         winget install --id $packageID --silent --no-upgrade
-        Write-Output "Exit code: $exitCode"
+        Write-Output "Exit code: $LASTEXITCODE"
 
         if ($?) {
             Write-Output "Package $packageID installed successfully."
-
-        } elseif ($exitCode -eq -1978335135) {
+            return 0
+        } elseif ($LASTEXITCODE -eq -1978335135) {
             Write-Output "Already installed."
-        } elseif ($exitCode -eq -1978335189) {
+        } elseif ($LASTEXITCODE -eq -1978335189) {
             Write-Output "Already installed and upgraded."
         } else {
             Write-Output "Failed to install package $packageID."
