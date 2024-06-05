9eda98dd20e777417ddb2f0cf69e10fdc1e3c068
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-05 15:41:44 +0300

    test

--- Resources/playbook-ansible-winget-ps.ps1
+++ Resources/playbook-ansible-winget-ps.ps1
@@ -39,7 +39,6 @@ function Check_If_Installed {
         [string]$packageID
     )
 
-    Write-Output "Checking $packageID..."
     $InstalledApps = winget list
     return $InstalledApps -match $packageID
 }
@@ -50,7 +49,6 @@ function Check_If_Updatable {
         [string]$packageID
     )
 
-    Write-Output "Checking $packageID..."
     return [int64] (winget list --id $packageID | Select-String '\bVersion\s+Available\b' -Quiet)
 }
 
@@ -60,22 +58,17 @@ function Install-Package {
         [string]$packageID
     )
 
-    Write-Output "Installing package $packageID..."
     if (-not (Check_If_Installed -packageID $packageID)) {
         winget install --id $packageID --silent --no-upgrade
         if ($LASTEXITCODE -eq 0) {
-            Write-Output "Package $packageID installed successfully."
             return $EXIT_CODE_SUCCESS
         } elseif ($LASTEXITCODE -eq -1978335135) {
-            Write-Output "Already installed."
             return $EXIT_CODE_ALREADY_PRESENT
         } else {
-            Write-Output "Failed to install package $packageID."
             return $EXIT_CODE_INSTALL_FAILED
         }
     }
     else {
-        Write-Output "Package $packageID is already Installed."
         return $EXIT_CODE_ALREADY_PRESENT
     }
 }
@@ -86,22 +79,17 @@ function Uninstall-Package {
         [string]$packageID
     )
 
-    Write-Output "Uninstalling package $packageID..."
     if (Check_If_Installed -packageID $packageID) {
         winget uninstall --id $packageID --silent
         if ($LASTEXITCODE -eq 0) {
-            Write-Output "Package $packageID uninstalled successfully."
             return $EXIT_CODE_SUCCESS
         } elseif ($LASTEXITCODE -eq -1978335212) {
-            Write-Output "Already uninstalled."
             return $EXIT_CODE_ALREADY_ABSENT
         } else {
-            Write-Output "Failed to uninstall package $packageID."
             return $EXIT_CODE_UNINSTALL_FAILED
         }
     }
     else {
-        Write-Output "Package $packageID is already Uninstalled."
         return $EXIT_CODE_ALREADY_ABSENT
     }
 }
@@ -112,42 +100,36 @@ function Update-Package {
         [string]$packageID
     )
 
-    Write-Output "Updating package $packageID..."
     if (Check_If_Updatable -packageID $packageID) {
         winget update --id $packageID --silent
         if ($LASTEXITCODE -eq 0) {
-            Write-Output "Package $packageID updated successfully."
             return $EXIT_CODE_SUCCESS
         } elseif ($LASTEXITCODE -eq -1978335189) {
-            Write-Output "Already updated."
             return $EXIT_CODE_SUCCESS
         } elseif ($LASTEXITCODE -eq -1978335212) {
-            Write-Output "This package is not installed."
             return $EXIT_CODE_ALREADY_ABSENT
         } else {
-            Write-Output "Failed to update package $packageID."
             return $EXIT_CODE_UPDATE_FAILED
         }
     }
     else {
-        Write-Output "Package $packageID is already updated."
         return $EXIT_CODE_SUCCESS
     }
 }
 
 # Запуск функций в сответствии с переданными параметрами
 if ($state -eq "present") {
-    $exitCode = (Install-Package -packageID $appID)
+    $exitCode = Install-Package -packageID $appID
 } elseif ($state -eq "absent") {
-    $exitCode = (Uninstall-Package -packageID $appID)
+    $exitCode = Uninstall-Package -packageID $appID
 } elseif ($state -eq "updated") {
-    $exitCode = (Update-Package -packageID $appID)
+    $exitCode = Update-Package -packageID $appID
 } else {
     Write-Output "Invalid state. Use 'present', 'absent' or 'updated'."
     $exitCode = $EXIT_CODE_INVALID_STATE
 }
 
 # Ensure exitCode is a single integer value
-$exitCode = [int]$exitCode[0]
+$exitCode = [int]$exitCode
 
 $host.SetShouldExit($exitCode)
