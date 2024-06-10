39f42bbcdd42f2c42a9543c9b1feaa0e9af6b527
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-10 06:26:12 +0300

    upd

--- Resources/ansible-winget.ps1
+++ Resources/ansible-winget.ps1
@@ -49,7 +49,7 @@ function Install-Package {
     )
 
     # Write-Output "Installing package $packageID..."
-    if (Check_If_Installed -packageID $appID) {
+    if (-not (Check_If_Installed -packageID $appID)) {
         $output = winget install --id $packageID --silent --no-upgrade
         if ($?) {
             Write-Output "Package $packageID installed successfully."
@@ -74,7 +74,7 @@ function Uninstall-Package {
     )
 
     # Write-Output "Uninstalling package $packageID..."
-    if (-not (Check_If_Installed -packageID $appID)) {
+    if (Check_If_Installed -packageID $appID) {
         $output = winget uninstall --id $packageID --silent
         if ($?) {
             Write-Output "Package $packageID uninstalled successfully."
