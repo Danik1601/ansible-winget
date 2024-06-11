5d9718cae4b5277b2b89ef1b82b65435a309f805
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-11 03:52:59 +0300

    upd

--- Resources/ansible-winget.ps1
+++ Resources/ansible-winget.ps1
@@ -32,6 +32,7 @@ function Check_If_Installed {
     # Write-Output "Checking $packageID..."
     winget list $packageID
     # return $InstalledApps -match $packageID
+    return $?
 }
 
 # Функция для проверки наличия обновления через Winget
