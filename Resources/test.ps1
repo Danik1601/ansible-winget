a73e657627ca55a8a3ed50e3bb93b051fb105cfc
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-12 21:11:27 +0300

    upd

--- Resources/test.ps1
+++ Resources/test.ps1
@@ -61,7 +61,7 @@ function Build_Command {
     $null {Write-Host "Error. State must not be NULL"}
     present { $process = "install" }
     absent { $process = "uninstall" }
-    updated { $process = "updated" }
+    updated { $process = "update" }
     else { Write-Host "Invalid state. Use 'present', 'absent' or 'updated'." }
     }   
 
