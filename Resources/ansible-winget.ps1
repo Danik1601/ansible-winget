ea3570da4c285ea7e7e6669125ff99bafd8160f1
Danik1601 <17238959+Danik1601@users.noreply.github.com>
2024-06-13 02:46:01 +0300

    test

--- Resources/ansible-winget.ps1
+++ Resources/ansible-winget.ps1
@@ -34,7 +34,6 @@ function Check_If_Installed {
 
     Write-Verbose "Checking $packageID..."
     $command = "winget list $packageID"
-    $output = Invoke-Expression $command
     return $?
 }
 
--- Resources/test.ps1
+++ Resources/test.ps1
@@ -34,7 +34,7 @@ function Check_If_Installed {
 
     Write-Verbose "Checking $packageID..."
     $command = "winget list $packageID"
-    $output = Invoke-Expression $command
+    Invoke-Command $command
     return $?
 }
 
